a8aae85c9770b4626d48dfcc0f49d72452407a6b
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 23:10:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |e|
+  planeteer_calls.find_all do |e|
      if e = valid_calls
      end
       

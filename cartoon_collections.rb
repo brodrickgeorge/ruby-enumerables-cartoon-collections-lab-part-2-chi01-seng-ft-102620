d07bb61607da2f3792571f8f571103e6a13a20a4
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 22:52:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.all? {|e| e.length > 4}
+  planeteer_calls.all? do |e| 
+    e.length > 4
+  end
 end
 
 def find_valid_calls(planeteer_calls)

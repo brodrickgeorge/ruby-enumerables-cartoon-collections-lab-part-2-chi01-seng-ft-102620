9eab38e4cf5a1470a516acc1629bc39144f987c4
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 22:53:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,6 @@ def summon_captain_planet(planeteer_calls)
     string.capitalize + "!"
    
   end
-  
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
 end
 
 def long_planeteer_calls(planeteer_calls)
